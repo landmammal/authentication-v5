ecd80d84a5037beff194bed0323b52a89356b943
landmammal <evolpirate@gmail.com>
2020-07-01 09:20:47 -0500

    adding passport for authentication

---
+++ app.js
@@ -0,0 +1,50 @@
+// Dependencies
+var express = require("express");
+var mysql = require("mysql");
+
+// app initialization
+var app = express();
+
+// set our view engine
+app.set("view engine", "ejs");
+
+// MIDDLEWARE -------------------
+app.use(express.static("public")); // adding static assets (css, img, js files)
+app.use(express.urlencoded({ extended: false })); // reads the data
+app.use(express.json()); // format the data coming in as an object under a property call body
+
+var PORT = process.env.PORT || 3000;
+
+// DATABASE Connection
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "authenticationV5",
+});
+
+db.connect(function (error) {
+  if (error) throw error;
+
+  console.log("MYSQL is connected");
+});
+
+// ############# ROUTES
+
+app.get("/", function (req, res) {
+  console.log("home route hit");
+  res.render("login-and-register.ejs");
+});
+
+// registering users
+app.post(
+  "/register",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+app.listen(PORT, function () {
+  console.log("Server is lit on PORT: " + PORT);
+});
